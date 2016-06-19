fece11c9b707801ec730ef7a841b31e132ddf94e
Sebastian Nepote <sebastian.nepote@helpling.com>
2016-06-19 19:44:25 +0200

    quicksort implementation for numbers

--- spec/service/quicksort_spec.rb
+++ spec/service/quicksort_spec.rb
@@ -3,8 +3,8 @@ require_relative '../../service/quicksort.rb'
 
 RSpec.describe Quicksort do
     describe '#sort?' do
-        context 'with 12454' do
-            it { expect(Quicksort.sort [1,2,4,5,3] ).to eql [1,2,3,4,5] }
+        context 'with [1,2,4,5,3]' do
+            it { expect(Quicksort.sort [5,3,4,1,2] ).to eql [1,2,3,4,5] }
         end
     end
 end
