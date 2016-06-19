a269be3a44caec0b0a9915fde9a4aa2793c85e8d
Sebastian Nepote <sebastian.nepote@helpling.com>
2016-06-19 19:56:05 +0200

    example for letters

--- spec/service/quicksort_spec.rb
+++ spec/service/quicksort_spec.rb
@@ -3,8 +3,12 @@ require_relative '../../service/quicksort.rb'
 
 RSpec.describe Quicksort do
     describe '#sort?' do
-        context 'with [1,2,4,5,3]' do
+        context 'with [5,3,4,1,2]' do
             it { expect(Quicksort.sort [5,3,4,1,2] ).to eql [1,2,3,4,5] }
+        end
+        letter_list = %w(c d a e b)
+        context "with #{letter_list}" do
+            it { expect(Quicksort.sort letter_list ).to eql %w(a b c d e) }
         end
     end
 end
